554577b2972b05fecef127e4652ec330487a0539
MNWake <theopkoester@icloud.com>
2024-02-01 10:47:01 -0500

    init

--- cwa/Cable/__init__.py
+++ cwa/Cable/__init__.py
@@ -1,4 +1,3 @@
-from .cable import Cable
 from .camera import DSLRCamera
 from .carrier import Carrier
 from .dc_motor import DCMotor
@@ -6,3 +5,4 @@ from .fork import Fork
 from .magazine import Magazine
 from .sensor import MagneticSensor
 from .steppermotor import StepperMotor
+from .cable import Cable
