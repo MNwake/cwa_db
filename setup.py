e080769f1b3bd8f6f3af8175ee992d1711cb60f2
MNWake <theopkoester@icloud.com>
2024-02-01 10:50:08 -0500

    init

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='CWA',
     version='0.1',
@@ -8,13 +11,7 @@ setup(
     description='Holds all the database files and the entire backend of our applications.',
     url='https://github.com/MNwake/cwa.git',
     packages=find_packages(),  # Update with your actual package name
-    install_requires=[
-        'certifi==2023.11.17',
-        'dnspython==2.5.0',
-        'mongoengine==0.27.0',
-        'pymongo==4.6.1',
-        'setuptools==69.0.3'
-    ],
+    install_requires=requirements,
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
